faa68265ddb9cfd2f0573bcfa095c10f43a9721d
imskanand <the.anand3099@gmail.com>
2022-07-30 14:31:34 +0530

    Basic of typescript is Complete

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,2 @@
 "use strict";
-const employees = {
-    id: 1,
-    name: 'Shubham',
-    retire: (date) => {
-        console.log(date);
-    },
-};
-console.log(employees);
-employees.name = 'jesus';
-employees.retire(new Date());
-console.log(employees);
 //# sourceMappingURL=index.js.map
