d5a46ea8f4aaf88427085cd62f5db59c8c273f74
imskanand <the.anand3099@gmail.com>
2022-07-30 13:31:48 +0530

    Problem with tuples in ts

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 console.log('Typescript Master!');
 let student = [1900268, 'Shubham'];
 console.log(student);
+student.push(2);
 student[0].toString();
 console.log(student[1].charAt(2));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,8 @@ console.log('Typescript Master!')
 let student: [number, string] = [1900268, 'Shubham']
 console.log(student)
 
+// Problem in tuples with Typescript
+student.push(2)
+
 student[0].toString()
 console.log(student[1].charAt(2))
