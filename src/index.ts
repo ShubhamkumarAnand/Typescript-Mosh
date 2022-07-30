b99500563d060263e3148dd140b88e3342c3606d
imskanand <the.anand3099@gmail.com>
2022-07-30 14:18:50 +0530

    Javascript Function Mess

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 // Properly annotate the variables and their return types
+// For making any parameters available optional parameters usually we do "taxYear?"
+// By default if we don't have any return parameters then function in javascript will return undefined.
 
 function calculateTax(income: number, taxYear = 2022): number {
 	if (income < 50_000 && taxYear < 2018) return income * 0.3
