7f26cd2da7456fe69a23e4cc12a95abf0c5f8cc4
imskanand <the.anand3099@gmail.com>
2022-07-30 13:59:41 +0530

    Added the Comments about the enums

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,5 @@
 "use strict";
 console.log('Typescript Master!');
-let small = 1;
-let medium = 2;
-let large = 3;
 let mySize = 2;
 console.log(mySize);
 let myBrotherSize = 1;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 console.log('Typescript Master!')
 
-// enums
-let small = 1
-let medium = 2
-let large = 3
+// enums -
+// let small = 1
+// let medium = 2
+// let large = 3
 
 // PascalCase
+
+/*
+  Making the enum constant make the js file more precise
+*/
 const enum Size {
-	Small = 1,
-	Medium,
+	Small = 1, // Default is 0
+	Medium, // Also string as well as all data type can be assigned
 	Large,
 }
 
