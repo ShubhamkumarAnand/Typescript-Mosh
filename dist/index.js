7161dc6de151a8fb54b0d067376b1ff17d3355a3
imskanand <the.anand3099@gmail.com>
2022-07-30 18:24:39 +0530

    Type Alias in Typescript

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@ let employee = {
     id: 1900268,
     eName: 'Snow White',
     joinedDate: (date) => {
-        console.log(date);
+        console.log(new Date(date.getTime()));
     },
 };
 console.log(employee);
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
-/*
-  Problem with the following code
-    - if we want to create another employee, we need to repeat the code !== DRY
-    - Too much jiggle in the code
-    - The other employee will have other shape which makes it inconsistent
-*/
+// Type Aliases uses the PascalCase naming convention
 
-let employee: {
+type Employee = {
 	readonly id: number
 	eName: string
 	joinedDate: (date: Date) => void
-} = {
+}
+
+let employee: Employee = {
 	id: 1_900_268,
 	eName: 'Snow White',
 	joinedDate: (date: Date) => {
-		console.log(date)
+		console.log(new Date(date.getTime()))
 	},
 }
 
