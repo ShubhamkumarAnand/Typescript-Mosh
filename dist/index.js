1b57624fc4b3849c85aea0b08cb3a9657841356a
imskanand <the.anand3099@gmail.com>
2022-07-30 11:20:57 +0530

    Debugging Experimentation

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log('Hi Maa!');
+let myAge = 21;
+if (myAge > 18) {
+    console.log('Ready to Drink & Drive!');
+    myAge += 1;
+}
+console.log(myAge);
+//# sourceMappingURL=index.js.map
