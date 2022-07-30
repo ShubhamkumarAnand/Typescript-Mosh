16076f2127b373297af387aaa122603e4ed89e4a
imskanand <the.anand3099@gmail.com>
2022-07-30 13:22:14 +0530

    Arrays, Code Compilation with Nodemon and typescript configuration

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 console.log('Typescript Master!')
 
-let num: number = 123_324_876
-let myName: string = 'Shubham'
-let isMarried = false
-console.log(isMarried)
-
-let levels
-
-function print(documents: any) {
-	console.log(documents)
-}
+let numbers: number[] = [1, 4, 2]
+numbers.forEach((n) => n.toString())
+console.log(numbers)
