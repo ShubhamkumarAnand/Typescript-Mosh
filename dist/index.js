56e739559efbc8c685cebfccbd43ac2d81b85db3
imskanand <the.anand3099@gmail.com>
2022-07-30 14:30:09 +0530

    Objects in Typescript

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,13 @@
 "use strict";
-function calculateTax(income, taxYear = 2022) {
-    if (income < 50000 && taxYear < 2018)
-        return income * 0.3;
-    return income * 0.4;
-}
-console.log(calculateTax(45000, 2017));
-console.log(calculateTax(55000, 2017));
+const employees = {
+    id: 1,
+    name: 'Shubham',
+    retire: (date) => {
+        console.log(date);
+    },
+};
+console.log(employees);
+employees.name = 'jesus';
+employees.retire(new Date());
+console.log(employees);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-// Properly annotate the variables and their return types
-// For making any parameters available optional parameters usually we do "taxYear?"
-// By default if we don't have any return parameters then function in javascript will return undefined.
-
-function calculateTax(income: number, taxYear = 2022): number {
-	if (income < 50_000 && taxYear < 2018) return income * 0.3
-	return income * 0.4
+// JavaScript object are expandable
+const employees: {
+	readonly id: number // Making this attribute not modified
+	name: string
+	retire: (date: Date) => void
+} = {
+	id: 1,
+	name: 'Shubham',
+	retire: (date: Date) => {
+		console.log(date)
+	},
 }
+console.log(employees)
 
-console.log(calculateTax(45_000, 2017))
-console.log(calculateTax(55_000, 2017))
+employees.name = 'jesus'
+employees.retire(new Date())
+console.log(employees)
