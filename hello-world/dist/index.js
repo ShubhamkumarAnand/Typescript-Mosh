e54e9f63ca74defc319c0198c63bfbe537638927
imskanand <the.anand3099@gmail.com>
2022-07-30 11:13:10 +0530

    Debugging the Typescript Files

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,3 +1,8 @@
 "use strict";
 console.log('Hi Maa!');
 let myAge = 21;
+if (myAge > 18) {
+    console.log('Ready to Drink & Drive!');
+    myAge += 1;
+}
+//# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,2 +1,7 @@
 console.log('Hi Maa!')
 let myAge: number = 21
+if (myAge > 18) {
+	console.log('Ready to Drink & Drive!')
+	myAge += 1
+}
+console.log(myAge)
