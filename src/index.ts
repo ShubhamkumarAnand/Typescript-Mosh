cd2c6b25cf3fe99c29b0c04c1d1a5bc06ee7605d
imskanand <the.anand3099@gmail.com>
2022-07-31 12:07:34 +0530

    Section 2 Ends

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,1 @@
-// Exercises
-type Users = {
-	name: string
-	age: number
-	occupation?: string
-}
-
-type Bird = {
-	fly: () => void
-}
-
-type Fish = {
-	swim: () => void
-}
-
-let pet: Bird | Fish
-
-type DayOfWeek = 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday'
+// Advanced Types in typescript completed
