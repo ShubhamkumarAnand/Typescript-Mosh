760b0ee3eec99a92b7c85622a5c4c186912a248b
imskanand <the.anand3099@gmail.com>
2022-08-01 13:47:57 +0530

    Index Signature property

--- dist/index.js
+++ dist/index.js
@@ -1,33 +1,9 @@
 "use strict";
-class Account {
-    id;
-    owner;
-    _balance;
-    nickname;
-    constructor(id, owner, _balance) {
-        this.id = id;
-        this.owner = owner;
-        this._balance = _balance;
-    }
-    deposit(amount) {
-        if (amount <= 0)
-            throw new Error('Amount must be greater than zero');
-        this._balance += amount;
-    }
-    get balance() {
-        return this._balance;
-    }
-    set balance(value) {
-        if (value <= 0)
-            throw new Error('Invalid Amount');
-        this._balance = value;
-    }
+class seatArrangements {
 }
-let account = new Account(1, 'Shubham', 0);
-console.log(account);
-console.log(typeof account);
-console.log(account instanceof Account);
-account.deposit(100);
-account.balance = 20000000;
-console.log(account.balance);
+let seat = new seatArrangements();
+seat.A1 = 'imskanand';
+seat['A2'] = 'Mosh';
+console.log(seat.A1);
+console.log(seat.A2);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,15 @@
-// Object Oriented programming
+// Index Signatures
 
-class Account {
-	nickname?: string
-
-	constructor(public readonly id: number, public owner: string, private _balance: number) {}
-
-	deposit(amount: number): void {
-		if (amount <= 0) throw new Error('Amount must be greater than zero')
-		this._balance += amount
-	}
-	// getter -> Method inside a class to set a property on the state
-	get balance(): number {
-		return this._balance
-	}
-
-	// setter -> Method inside a class to set a property on the state
-	set balance(value: number) {
-		if (value <= 0) throw new Error('Invalid Amount')
-		this._balance = value
-	}
+class seatArrangements {
+	// A1,A2,A3,A4...
+	// Mosh, Jha, Spa,Her...
+	// Index Signature property
+	[seatNumber: string]: string
 }
 
-let account = new Account(1, 'Shubham', 0)
+let seat = new seatArrangements()
+seat.A1 = 'imskanand'
+seat['A2'] = 'Mosh'
 
-console.log(account)
-console.log(typeof account)
-console.log(account instanceof Account)
-
-account.deposit(100)
-account.balance = 20_000_000
-console.log(account.balance)
+console.log(seat.A1)
+console.log(seat.A2)
