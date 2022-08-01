2f12228082502b8bea4fa1ade6a913ac4fc02e4a
imskanand <the.anand3099@gmail.com>
2022-08-01 12:06:23 +0530

    OOP's & Functional Programming

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-// Advanced Types in typescript completed
+// Object Oriented Programming in typescript
+
+/*
+  - The typescript follows the OOPs as well as the Functional Programming
+  - The OOPs follows the principal of all the code can be treated as object which work together and object interactions happens to complete the tasks
+  - The Object contains the state/Attributes and Behavior/Methods
+  - Object Person can have attributes -> name, email and Behavior -> talk(), sing(), dance()
+*/
