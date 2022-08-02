8fd0dfd648e3c954c77d5e068de3520165d10059
imskanand <the.anand3099@gmail.com>
2022-08-02 15:57:34 +0530

    Module Completed

--- dist/index.js
+++ dist/index.js
@@ -1,29 +1,2 @@
 "use strict";
-class Logger {
-    fileName;
-    constructor(fileName) {
-        this.fileName = fileName;
-    }
-    write(message) {
-        console.log(message);
-    }
-}
-class Person {
-    firstName;
-    lastName;
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    get fullName() {
-        return this.firstName + ' ' + this.lastName;
-    }
-}
-class Employee extends Person {
-    salary;
-    constructor(salary, firstName, lastName) {
-        super(firstName, lastName);
-        this.salary = salary;
-    }
-}
 //# sourceMappingURL=index.js.map
