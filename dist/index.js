bca7a3710ae1313a2f4e67d6eb58609678db7540
imskanand <the.anand3099@gmail.com>
2022-08-03 12:23:57 +0530

    Class as Constraint

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,16 @@
 "use strict";
+class Person {
+    name;
+    constructor(name) {
+        this.name = name;
+    }
+}
+class Customer extends Person {
+}
 function echo(value) {
     console.log(value);
     return value;
 }
-console.log(echo({ name: 'Hello All!' }));
+console.log(echo(new Person('Mosh')));
+console.log(echo(new Customer('Mosh Pro')));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 // Generic Constraints -> Limiting the type of the value which can be accepted
 
-interface Person {
-	name: string
+class Person {
+	constructor(public name: string) {}
 }
+
+class Customer extends Person {}
 
 function echo<T extends Person>(value: T): T {
 	console.log(value)
 	return value
 }
 
-console.log(echo({ name: 'Hello All!' }))
+console.log(echo(new Person('Mosh')))
+console.log(echo(new Customer('Mosh Pro')))
 // console.log(echo(2))
