2e1922ad661feab55df4698fdb3e95dea3592703
imskanand <the.anand3099@gmail.com>
2022-08-03 10:41:21 +0530

    Generics in Typescript

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,20 @@
 "use strict";
+class KeyValuePair {
+    key;
+    value;
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+let hash = new KeyValuePair(1, 'AA');
+class StringKeyValuePair {
+    key;
+    value;
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+let stringHash = new StringKeyValuePair('1', 'AAA');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-// Class Interface and OOPs in Typescript completed.
+// Generics in Typescript -> Try to solve the common problem related to the types of attributes.
+
+class KeyValuePair {
+	constructor(public key: number, public value: string) {}
+}
+
+let hash = new KeyValuePair(1, 'AA')
+
+class StringKeyValuePair {
+	constructor(public key: string, public value: string) {}
+}
+
+let stringHash = new StringKeyValuePair('1', 'AAA')
