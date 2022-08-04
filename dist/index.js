0bbd5f31d5a161f6ca03d7bd78a8c2bcfe5ad058
imskanand <the.anand3099@gmail.com>
2022-08-04 11:44:54 +0530

    Generic Secton Completed

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,2 @@
 "use strict";
-function echo(value) {
-    return value;
-}
-function printName(obj) {
-    console.log(obj.name);
-}
-class Entity {
-    id;
-    constructor(id) {
-        this.id = id;
-    }
-}
 //# sourceMappingURL=index.js.map
